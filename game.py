708ddf2d4a58ae696a3977f4a171fc3b197e33ba
niysi <108881519+niysi@users.noreply.github.com>
2023-05-19 12:34:37 +0000

    Pending changes exported from your codespace

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 from random import randint
+import os
+import platform
 from player import Player, Health_bar
 from enemies import Fight1_stats, Fight2_stats, Fight3_stats, Allfights
 from storytelling import story_telling
-import os
-import platform
 # Clear console to make our game prettier
 
 
@@ -18,13 +18,13 @@ def clear_console():
 print(story_telling[0])
 
 
-
+# asda
 def Menu(p):
     while True:
         print("1- Continue")
         print("2- Check Statistics")
 
-        n = input("Go Traveller")
+        n = input("Go Traveller: ")
 
         try:
 
@@ -32,7 +32,7 @@ def Menu(p):
                 Menu_Fight(p)
             if n == "2":
                 # Hasn't finished yet.
-                Menu_Statistics(p)
+                Menu_Statistics()
             else:
                 print("Will you choose?")
         except (NameError, SyntaxError):
@@ -55,30 +55,30 @@ def Menu_Fight(p):
             if Allfights[j] == Fight1_stats:
                 Fight1_stats.hp = Fight1_stats.hp - Player.base_dmg
                 print(f"You striked the enemy, he has {Fight1_stats.hp}")
-                Player.hp -= Fight1_stats.dmg
-                print(f"You was striked by enemy, your {Player.hp}")
+                Player.base_hp -= Fight1_stats.dmg
+                print(f"You was striked by enemy, your {Player.base_hp}")
             print("----------------------------------")
             if Allfights[j] == Fight2_stats:
                 Fight2_stats.hp = Fight2_stats.hp - Player.base_dmg
                 print(f"You striked the enemy, he has {Fight2_stats.hp}")
-                Player.hp -= Fight2_stats.dmg
-                print(f"You was striked by enemy, your {Player.hp}")
+                Player.base_hp -= Fight2_stats.dmg
+                print(f"You was striked by enemy, your {Player.base_hp}")
             print("----------------------------------")
 
             if Allfights[j] == Fight3_stats:
                 Fight3_stats.hp = Fight3_stats.hp - Player.base_dmg
                 print(f"You striked the enemy, he has {Fight3_stats.hp}")
-                Player.hp -= Fight1_stats.dmg
-                print(f"You was striked by enemy, your {Player.hp}")
+                Player.base_hp -= Fight1_stats.dmg
+                print(f"You was striked by enemy, your {Player.base_hp}")
             print("----------------------------------")
 
             if n == "2":
-                Player.hp += randint(10, 20)
-                if Player.hp > 100:
-                    Player.hp = 100
-                    print(f"You healed {Player.hp}")
+                Player.base_hp += randint(10, 20)
+                if Player.base_hp > 100:
+                    Player.base_hp = 100
+                    print(f"You healed {Player.base_hp}")
 
-        if Player.hp < 0:
+        if Player.base_hp < 0:
             print("Here is end of your story traveller...")
             break
         if Fight1_stats.hp < 0 and j == 0:
@@ -96,7 +96,7 @@ def Menu_Fight(p):
             print(story_telling[i])
             break
 
-# def Menu_Statistics(p)
-
-
+def Menu_Statistics():
+    print("This is not working")
+#This is not working
 Menu(Player)
